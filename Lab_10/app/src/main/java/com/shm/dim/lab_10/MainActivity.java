bb531835b4df8ff11266d65d0f62cc863ded313b
Dima Shumanski <dima.shumanski@gmail.com>
2018-04-09 12:33:49 +0300

    Update Lab_10

--- Lab_10/app/src/main/java/com/shm/dim/lab_10/MainActivity.java
+++ Lab_10/app/src/main/java/com/shm/dim/lab_10/MainActivity.java
@@ -1,44 +1,60 @@
 package com.shm.dim.lab_10;
 
+import android.content.ContentResolver;
+import android.database.Cursor;
+import android.net.Uri;
+import android.os.Bundle;
 import android.speech.tts.TextToSpeech;
 import android.support.v7.app.AppCompatActivity;
-import android.os.Bundle;
-import android.util.Log;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.EditText;
-import android.widget.RadioButton;
+import android.widget.ListView;
 import android.widget.RadioGroup;
-import android.widget.Spinner;
-import android.widget.Toast;
+import android.widget.TextView;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Locale;
 
 public class MainActivity extends AppCompatActivity {
-    
+
+    private ListView mSmsList;
     private EditText mText;
     private RadioGroup mLanguageGroup;
     private TextToSpeech textToSpeech;
-
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        mSmsList = findViewById(R.id.sms_list);
         mText = findViewById(R.id.text);
         mLanguageGroup = findViewById(R.id.language_group);
+
+        ArrayAdapter<String> adapter = new ArrayAdapter<>(this,
+                android.R.layout.simple_list_item_1, getAllSms());
+        mSmsList.setAdapter(adapter);
+        mSmsList.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                String smsText = ((TextView) view).getText().toString();
+                mText.setText(smsText);
+            }
+        });
+
+        mLanguageGroup.setOnCheckedChangeListener(new RadioGroup.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(RadioGroup group, int checkedId) {
+                setTextToSpeechLanguage();
+            }
+        });
 
         textToSpeech = new TextToSpeech(getApplicationContext(), new TextToSpeech.OnInitListener() {
             @Override
             public void onInit(int status) {
-                setTextToSpeechLanguage();
-            }
-        });
-        mLanguageGroup.setOnCheckedChangeListener(new RadioGroup.OnCheckedChangeListener() {
-            @Override
-            public void onCheckedChanged(RadioGroup group, int checkedId) {
                 setTextToSpeechLanguage();
             }
         });
@@ -53,6 +69,22 @@ public class MainActivity extends AppCompatActivity {
         super.onPause();
     }
 
+
+    public List<String> getAllSms() {
+        List<String> listSms = new ArrayList<>();
+
+        ContentResolver cr = this.getContentResolver();
+
+        Cursor c = cr.query(Uri.parse("content://sms/"), null, null, null, null);
+        if (c != null) {
+            while(c.moveToNext()) {
+                listSms.add(c.getString(c.getColumnIndexOrThrow("body")));
+            }
+            c.close();
+        }
+
+        return listSms;
+    }
 
     private Locale getUserSelectedLanguage() {
         int checkedRadioId = mLanguageGroup.getCheckedRadioButtonId();
